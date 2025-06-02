90d4b7d618d842bc8be7d2734e92015cab8a2489
Khairul Bashar Naeeim <mdkhairulb01@gmail.com>
2025-06-02 21:15:03 +0530

    added some comments

--- index.js
+++ index.js
@@ -71,6 +71,7 @@ const verifyTokenEmail = (req, res, next) => {
     next();
 }
 
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.plgxbak.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
